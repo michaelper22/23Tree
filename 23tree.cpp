a92b62dfaf81b70d01fc5eacc98fce99cfe832ff
michaelper22 <michaelper22@gmail.com>
2013-11-06 09:33:39 -0500

    Wrote cases 0 and 1 in Tree::insert() method

--- 23tree.cpp
+++ 23tree.cpp
@@ -6,13 +6,14 @@ class Node{
 	public:
 	
 	Node* parent;
-	Node* key1;
-	Node* key2;
+	int key1;
+	int key2;
 	Node* child1;
 	Node* child2;
 	Node* child3;
 	
-	Node(Node* newParent, Node* newKey1, Node* newKey2, Node* newChild1, Node* newChild2, Node* newChild3){
+	// Constructor for internal node
+	Node(Node* newParent, int newKey1, int newKey2, Node* newChild1, Node* newChild2, Node* newChild3){
 		parent = newParent;
 		key1 = newKey1;
 		key2 = newKey2;
@@ -20,6 +21,13 @@ class Node{
 		child2 = newChild2;
 		child3 = newChild3;
 	} //Node()
+	
+	// Constructor for leaf node
+	Node(Node* newParent, int data){
+		parent = newParent;
+		key1 = -1;
+		key2 = data;
+	}//Node()
 	
 	void printNode(){
 	
@@ -37,6 +45,22 @@ class Tree{
 	
 	void insert(int data){
 		Node* spot = findSpot(root, data);
+		
+		
+		// Case 0: Spot has 0 children
+		if(spot->child1 == NULL && spot->child2 == NULL && spot->child3 == NULL)
+			spot->child1 = new Node(spot, data);
+			
+		// Case 1: Spot has 1 child
+		if(spot->child1 != NULL && spot->child2 == NULL && spot->child3 == NULL){
+			if(spot->child1->key2 <= data)
+				spot->child2 = new Node(spot, data);
+			else {
+				spot->child2 = spot->child1;
+				spot->child1 = new Node(spot, data);
+			}
+		}
+			
 	}//insert()
 	
 	void printTree(Node* nodeToPrint){
@@ -56,8 +80,6 @@ class Tree{
 		
 		else if(data >= currentNode->key2 && key2 == -1)
 			return findSpot(currentNode->child3, data);
-			
-	}
-	
+	}//findSpot()
 };//Tree class
 
